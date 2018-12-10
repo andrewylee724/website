19e4213ac6d8f9fed6085cb8c95e32fc91ef2ee9
Andrew Lee <Andrew.Lee2@bjss.com>
2018-12-10 15:22:57 -0500

    Updates

---
+++ precache-manifest.e23925f0a5110b1e879aa35b4fbcc033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "30b77f2b3d77c8ccfc2f",
+    "url": "/website/static/js/main.30b77f2b.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/website/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "30b77f2b3d77c8ccfc2f",
+    "url": "/website/static/css/main.082ad99d.chunk.css"
+  },
+  {
+    "revision": "2ac94e46b91e5039f63d6d0d481c89d9",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65e7c994a751d95b8c60676f0d023580.js"
+  "/website/precache-manifest.e23925f0a5110b1e879aa35b4fbcc033.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
